ca483c10460e6908e8e7d54b6e1f7606699fd828
Jonathan Nguyen <jonathanguven@gmail.com>
2024-10-09 21:26:44 -0700

    initial commit

---
+++ src/agent/Agent.ts
@@ -0,0 +1,15 @@
+export class Agent {
+  private name: string;
+  private address: string;
+  // private protocols: Protocol[];
+  // private storage: Storage;
+  // private context: Context;
+
+  constructor(name: string, storage: Storage) {
+    this.name = name;
+    // this.address = this.generateAddress();
+    // this.protocols = [];
+    // this.storage = storage;
+    // this.context = new Context(this);
+  }
+}
